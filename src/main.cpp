fbf002945f2def3a237ce272e1280c364fb11b3e
NicolasMelet <nicolas.melet@epitech.eu>
2024-05-14 09:53:48 +0200

    feat: get arguments

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,35 @@
 
 #include "define.hpp"
 #include "Reception.hpp"
+#include <iostream>
 
-int main(void)
+int getArgs(char **av)
 {
+    std::size_t idx = 0;
+
+    std::string current = av[1];
+    double cookingTimeMult = std::stod(current, &idx);
+    if (idx != current.size())
+        return FAILURE;
+
+    current = av[2];
+    int cookPerKitchen = std::stoi(current, &idx);
+    if (idx != current.size())
+        return FAILURE;
+
+    current = av[3];
+    std::size_t replacementCD = std::stol(current, &idx);
+    if (idx != current.size())
+        return FAILURE;
+
+    Plazza::Reception reception(cookingTimeMult, cookPerKitchen, replacementCD);
     return SUCCESS;
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 4)
+        return FAILURE;
+
+    return getArgs(av);
 }
